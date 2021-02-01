61e5b211554135f6caa9ea4fd385981b560b7360
jayeom-fitz <djawjdgus93@naver.com>
2021-02-01 22:00:42 +0900

    #0-3.1 Add Other Sidebar Items

--- src/sidebar/Sidebar.js
+++ src/sidebar/Sidebar.js
@@ -6,16 +6,25 @@ import SidebarRow from './SidebarRow';
 import HomeIcon from "@material-ui/icons/Home";
 import WhatshotIcon from "@material-ui/icons/Whatshot";
 import SubscriptionsIcon from "@material-ui/icons/Subscriptions";
+import VideoLibraryIcon from "@material-ui/icons/VideoLibrary";
+import OndemandVideoIcon from "@material-ui/icons/OndemandVideo";
+import WatchLaterIcon from "@material-ui/icons/WatchLater";
+import ThumbUpAltOutlinedIcon from "@material-ui/icons/ThumbUpAltOutlined";
 
 function Sidebar() {
   return (
     <div className="sidebar">
-      <SidebarRow selected Icon={HomeIcon} title="Home"/>
-      <SidebarRow Icon={WhatshotIcon} title="Trending"/>
-      <SidebarRow Icon={SubscriptionsIcon} title="Subscription"/>
+      <SidebarRow selected Icon={HomeIcon} title="홈"/>
+      <SidebarRow Icon={WhatshotIcon} title="인기"/>
+      <SidebarRow Icon={SubscriptionsIcon} title="구독"/>
 
       <hr />
 
+      <SidebarRow Icon={VideoLibraryIcon} title="보관함"/>
+      <SidebarRow Icon={OndemandVideoIcon} title="내 재생 목록"/>
+      <SidebarRow Icon={WatchLaterIcon} title="나중에"/>
+      <SidebarRow Icon={ThumbUpAltOutlinedIcon} title="좋아요"/>
+          
     </div>
   )
 }
