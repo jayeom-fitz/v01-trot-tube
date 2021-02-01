0c7f7d9e35f902d66aff806a6f65241baf12b01c
jayeom-fitz <djawjdgus93@naver.com>
2021-02-01 21:26:00 +0900

    #0-3 Create App's Sidebar and Add CSS

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import React from 'react';
 import './App.css';
 
 import Header from './header/Header';
+import Sidebar from './sidebar/Sidebar';
+import RecommendedVideos from './videos/RecommendedVideos';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      
+
+      <div className="app__page">
+        <Sidebar />
+        <RecommendedVideos />
+      </div>
+
       <h1>Hello JayEom</h1><h1>Hello JayEom</h1><h1>Hello JayEom</h1>
       <h1>Hello JayEom</h1><h1>Hello JayEom</h1><h1>Hello JayEom</h1>
       <h1>Hello JayEom</h1><h1>Hello JayEom</h1><h1>Hello JayEom</h1>
---
+++ src/sidebar/Sidebar.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Sidebar.css'
+
+import SidebarRow from './SidebarRow';
+
+import HomeIcon from "@material-ui/icons/Home";
+import WhatshotIcon from "@material-ui/icons/Whatshot";
+import SubscriptionsIcon from "@material-ui/icons/Subscriptions";
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <SidebarRow selected Icon={HomeIcon} title="Home"/>
+      <SidebarRow Icon={WhatshotIcon} title="Trending"/>
+      <SidebarRow Icon={SubscriptionsIcon} title="Subscription"/>
+
+      <hr />
+
+    </div>
+  )
+}
+
+export default Sidebar
