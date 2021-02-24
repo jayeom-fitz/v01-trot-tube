643b9c1d015abea899f54246b6149016ef8dc72c
jayeom-fitz <djawjdgus93@naver.com>
2021-02-25 00:43:02 +0900

    #1-5-8 Home Slider Management part 3

--- src/components/edition/tv-program/TvProgramsSlider.js
+++ src/components/edition/tv-program/TvProgramsSlider.js
@@ -11,6 +11,7 @@ import { AiFillLeftSquare,
 
 function TvProgramsSlider() {
   const [loaded, setLoaded] = useState(false);
+  const [value, setValue] = useState(0);
 
   const [lefts, setLefts] = useState([]);
   const [rights, setRights] = useState([]);
@@ -31,7 +32,7 @@ function TvProgramsSlider() {
     }
 
     getTvPrograms();
-  }, [])
+  }, [value])
 
   const onCheckClick = e => {
     const id = e.target.id;
@@ -56,33 +57,55 @@ function TvProgramsSlider() {
     const id = e.target.id;
 
     if(id[0] === 'R') {
-      var idx = rights.length;
-
-      const arr = lefts.filter(left => left.checked);
-      setRights(rights.concat(arr.map(left => {
-        return {
-          value: left.value,
-          checked: false,
-          index: ++idx
-        }
-      })));
-      
-      console.log(rights);
-
+      setRights(rights.concat(lefts.filter(left => left.checked)));
+      setLefts(lefts.filter(left => !left.checked));
     } else {
-      
+      setLefts(lefts.concat(rights.filter(right => right.checked)));
+      setRights(rights.filter(right => !right.checked));
     }
   };
 
   const onVerticalClick = e => {
     const id = e.target.id;
+    if(rights.length === 0) return;
+    var arr = rights.slice();
 
     if(id[0] === 'U') {
-      
+      if(rights[0].checked) return;
+      for(var i=1; i<arr.length; i++) {
+        if(arr[i].checked) {
+          const v = arr[i - 1];
+          arr[i - 1] = arr[i];
+          arr[i] = v;
+        }
+      }
     } else {
-      
+      if(rights[rights.length - 1].checked) return;
+      for(var i=arr.length-2; i>=0; i--) {
+        if(arr[i].checked) {
+          const v = arr[i + 1];
+          arr[i + 1] = arr[i];
+          arr[i] = v;
+        }
+      }
     }
+    setRights(arr);
   };
+
+  const onSave = () => {
+    var index = 1;
+    lefts.filter(left => left.sliderIndex !== 0).map(async function (v) {
+      await storeService.collection('tv-programs').doc(v.id).update({
+        sliderIndex: 0
+      })
+    })
+    rights.map(async function (v) {
+      await storeService.collection('tv-programs').doc(v.id).update({
+        sliderIndex: index++
+      })
+    })
+    alert('저장되었습니다.')
+  }
 
   return (
     <>
@@ -90,7 +113,16 @@ function TvProgramsSlider() {
         <div style={{width:'100%'}}>
           <Container>
             <Title>홈 슬라이드</Title>
-            
+
+            <Button 
+              color='#DC3545'
+              onClick={() => setValue(value + 1)
+              }>취소</Button>
+            <Button 
+              color='#007BFF'
+              onClick={onSave}
+              >저장</Button>
+
             <SliderContainer>
               <Box>
                 {lefts.map((v) => 
@@ -106,6 +138,7 @@ function TvProgramsSlider() {
                 <LeftArrow id="LeftArrow" onClick={onHorizontalClick} size='48'/>
               </ButtonBox>
               <Box>
+                <BoxTitle>홈 슬라이드 구성</BoxTitle>
                 {rights.map((v) => 
                   <Content key={`R${v.id}`} active={v.checked}>
                     <Image 
@@ -134,14 +167,43 @@ const Container = styled.div`
 const Title = styled.h1`
   margin: 0;
 `
+const Button = styled.button`
+  width: 75px;
+  height: 50px;
+  margin: 20px 20px 0 20px;
+  font-weight: bold;
+
+  border: 1px solid ${(props) => props.color};
+  transition-duration: 0.4s;
+  background-color: white;
+
+  &:hover {
+    color: white;
+    background-color: ${(props) => props.color};
+  }
+
+  &:focus {
+    outline: none;
+    color: white;
+    background-color: ${(props) => props.color};
+  }
+`
 const SliderContainer = styled.div`
   display: flex;
   margin: 20px 0;
 `
 const Box = styled.div`
   flex: 0.5;
+  height: 500px;
   border: 1px solid lightgrey;
   margin: 10px;
+  overflow-y: auto;
+`
+const BoxTitle = styled.h4`
+  margin: 0;
+  border: 1px solid lightgrey;
+  margin: 10px;
+  text-align: center;
 `
 const ButtonBox = styled.div`
   display: flex;
@@ -152,33 +214,65 @@ const Content = styled.div`
   padding: 10px;
   text-align: center;
   
-  transition: 0.8s;
+  transition: 0.3s;
   ${({active}) => active && `
     background-color: lightgrey;
   `};
 `
 const Image = styled.img`
   width: 50%;
-  height: 50%;
+  max-height: 150px;
   cursor: pointer;
 `
 const LeftArrow = styled(AiFillLeftSquare)`
   display: flex;
   margin: 10px 0;
   cursor: pointer;
+
+  path {
+    pointer-events: none;
+  }
+
+  &:hover {
+    color : red;
+  }
 `
 const RightArrow = styled(AiFillRightSquare)`
   display: flex;
   margin: 10px 0;
   cursor: pointer;
+
+  path {
+    pointer-events: none;
+  }
+
+  &:hover {
+    color : red;
+  }
 `
 const UpArrow = styled(AiFillUpSquare)`
   display: flex;
   margin: 10px 0;
   cursor: pointer;
+
+  path {
+    pointer-events: none;
+  }
+
+  &:hover {
+    color : red;
+  }
 `
 const DownArrow = styled(AiFillDownSquare)`
   display: flex;
   margin: 10px 0;
   cursor: pointer;
+
+  path {
+    pointer-events: none;
+  }
+
+  &:hover {
+    color : red;
+  }
 `
