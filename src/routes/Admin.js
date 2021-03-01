22382905b909a5f5313e3918f699494aade177a3
jayeom-fitz <djawjdgus93@naver.com>
2021-03-01 23:05:27 +0900

    #1-6-5 Clear TV and People

--- src/routes/Admin.js
+++ src/routes/Admin.js
@@ -1,5 +1,4 @@
 import React from 'react'
-
 import { useParams } from "react-router-dom";
 
 import Sidebar from '../components/admin/sidebar/Sidebar'
@@ -7,8 +6,11 @@ import Sidebar from '../components/admin/sidebar/Sidebar'
 import Main from '../components/admin/Main'
 
 import TvProgram from '../components/admin/tv-program/TvProgram';
+import TvPrograms from '../components/admin/tv-program/TvPrograms';
 import TvProgramSlider from '../components/admin/tv-program/TvProgramSlider';
+
 import People from '../components/admin/people/People';
+import Person from '../components/admin/people/Person';
 
 function Admin(props) {
   const { startComponent } = useParams();
@@ -20,7 +22,10 @@ function Admin(props) {
   const switchComponent = (prop) => {
     switch(prop) {
       case 'tv-program' : return <TvProgram />
+      case 'tv-programs' : return <TvPrograms />
       case 'tv-program-slider' : return <TvProgramSlider />
+
+      case 'person' : return <Person />
       case 'people' : return <People />
       default : return <Main user={props.user} />
     }
